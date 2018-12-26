745cd264338878a25ec2b860e4ef73696dafec1c
Or <or15yakov@gmail.com>
2018-12-26 15:51:06 +0200

    aaa

--- ConnectCommand.h
+++ ConnectCommand.h
@@ -1,23 +1,23 @@
+////
+//// Created by noa on 12/25/18.
+////
+//#include "Command.h"
 //
-// Created by noa on 12/25/18.
+//#ifndef MILESTONE1_CONNECTCOMMAND_H
+//#define MILESTONE1_CONNECTCOMMAND_H
 //
-#include "Command.h"
-
-#ifndef MILESTONE1_CONNECTCOMMAND_H
-#define MILESTONE1_CONNECTCOMMAND_H
-
-
-class ConnectCommand: public Command {
-    string ip;
-    int port;
-public:
-    ConnectCommand(string ip, int port);
-
-    virtual double doCommand();
-
-    void sendData(string path, double value);
-
-};
-
-
-#endif //MILESTONE1_CONNECTCOMMAND_H
+//
+//class ConnectCommand: public Command {
+//    string ip;
+//    int port;
+//public:
+//    ConnectCommand(string ip, int port);
+//
+//    virtual double doCommand();
+//
+//    void sendData(string path, double value);
+//
+//};
+//
+//
+//#endif //MILESTONE1_CONNECTCOMMAND_H
